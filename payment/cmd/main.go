b9ea89886aab9078d36382124716aec9adf4e894
Artem <dadiaajsduia@gmail.com>
2026-03-17 03:21:37 +0500

    Сгенерировал из прото файла и сделал логику сервера, написал опен апи контракт

--- payment/cmd/main.go
+++ payment/cmd/main.go
@@ -1,1 +1,168 @@
 package payment
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+
+	inventory_v1 "github.com/ChopX4/raketka/shared/pkg/proto/inventory/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
+)
+
+const grpcPort = 50051
+
+type InventoryService struct {
+	inventory_v1.UnimplementedInventoryServiceServer
+
+	mu    sync.RWMutex
+	parts map[string]*inventory_v1.Part
+}
+
+func (s *InventoryService) GetPart(_ context.Context, req *inventory_v1.GetPartRequest) (*inventory_v1.GetPartResponse, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	part, ok := s.parts[req.GetUuid()]
+	if !ok {
+		return nil, status.Errorf(codes.NotFound, "part with UUID %s not found", req.GetUuid())
+	}
+
+	return &inventory_v1.GetPartResponse{
+		Part: part,
+	}, nil
+}
+
+func (s *InventoryService) ListParts(_ context.Context, req *inventory_v1.ListPartsRequest) (*inventory_v1.ListPartsResponse, error) {
+	filter := req.GetFilter()
+
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	storage := make([]*inventory_v1.Part, 0)
+
+	for _, part := range s.parts {
+		if filter == nil {
+			storage = append(storage, part)
+			continue
+		}
+
+		if len(filter.GetUuids()) > 0 {
+			found := false
+			for _, v := range filter.GetUuids() {
+				if part.GetUuid() == v {
+					found = true
+					break
+				}
+			}
+			if !found {
+				continue
+			}
+		}
+
+		if len(filter.GetNames()) > 0 {
+			found := false
+			for _, v := range filter.GetNames() {
+				if part.GetName() == v {
+					found = true
+					break
+				}
+			}
+			if !found {
+				continue
+			}
+		}
+
+		if len(filter.GetCategories()) > 0 {
+			found := false
+			for _, v := range filter.GetCategories() {
+				if part.GetCategory() == v {
+					found = true
+					break
+				}
+			}
+			if !found {
+				continue
+			}
+		}
+
+		if len(filter.GetManufacturerCountries()) > 0 {
+			found := false
+			for _, v := range filter.GetManufacturerCountries() {
+				if part.Manufacturer.GetCountry() == v {
+					found = true
+					break
+				}
+			}
+			if !found {
+				continue
+			}
+		}
+
+		if len(filter.GetTags()) > 0 {
+			found := false
+			for _, v := range filter.GetTags() {
+				for _, tag := range part.GetTags() {
+					if tag == v {
+						found = true
+						break
+					}
+				}
+			}
+			if !found {
+				continue
+			}
+		}
+
+		storage = append(storage, part)
+	}
+
+	return &inventory_v1.ListPartsResponse{
+		Parts: storage,
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Printf("failed to listen: %v\n", err)
+		return
+	}
+	defer func() {
+		if cerr := lis.Close(); cerr != nil {
+			log.Printf("failed to close: %v\n", cerr)
+		}
+	}()
+
+	s := grpc.NewServer()
+	service := &InventoryService{
+		parts: make(map[string]*inventory_v1.Part),
+	}
+
+	inventory_v1.RegisterInventoryServiceServer(s, service)
+
+	reflection.Register(s)
+
+	go func() {
+		log.Printf("🚀 gRPC server listening on %d\n", grpcPort)
+		err := s.Serve(lis)
+		if err != nil {
+			log.Printf("failed to serve: %v\n", err)
+			return
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("🛑 Shutting down gRPC server...")
+	s.GracefulStop()
+	log.Println("✅ Server stopped")
+}
